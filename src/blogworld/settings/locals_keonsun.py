fa79be897774472d78b128178e0057c3787b17ad
Keonsun Kim <keonsunkim@gmail.com>
2018-12-04 14:26:24 +0900

    Lesson 1 Django Configurations

--- src/blogworld/settings/locals_keonsun.py
+++ src/blogworld/settings/locals_keonsun.py
@@ -11,24 +11,25 @@ with open(secret_file) as f:
     secrets = json.loads(f.read())
 
 
-def get_secret(setting, secrets=secrets):
+def get_secret(setting, secrets):
     try:
         return secrets[setting]
     except KeyError:
         error_msg = f"Set the {setting} environment variable"
         raise ImproperlyConfigured(error_msg)
+
 # Database
 # https://docs.djangoproject.com/en/1.11/ref/settings/#databases
 
 
-SECRET_KEY = get_secret("SECRET_KEY")
+SECRET_KEY = get_secret('SECRET_KEY', secrets)
 
 DATABASES = {
     'default': {
         'ENGINE': 'django.db.backends.postgresql_psycopg2',
-        'NAME': get_secret("DB_NAME"),
-        'USER': get_secret("DB_USERNAME"),
-        'PASSWORD': get_secret("DB_PASSWORD"),
+        'NAME': get_secret("DB_NAME", secrets),
+        'USER': get_secret("DB_USERNAME", secrets),
+        'PASSWORD': get_secret("DB_PASSWORD", secrets),
         'HOST': 'localhost',
         'PORT': '5432',
     }
